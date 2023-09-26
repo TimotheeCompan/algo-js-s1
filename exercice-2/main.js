c84d5f2b60b18e9429508f8e359067cfe403ffb6
TimotheeCompan <146062411+TimotheeCompan@users.noreply.github.com>
2023-09-26 12:59:30 +0200

    Add files via upload

---
+++ exercice-2/main.js
@@ -0,0 +1,14 @@
+var a = 4
+var b = 3
+var c = 3
+
+switch(a){
+    case b:
+        console.log("a égal b")
+        break;
+    case c:
+        console.log("a égal c")
+        break;
+    default:
+        console.log("égal a rien")
+}
---
+++ exercice-3/main.js
@@ -0,0 +1,4 @@
+var a= 6
+for(let i= 0; i < 6; i++ ){
+    console.log("oklm")
+}
